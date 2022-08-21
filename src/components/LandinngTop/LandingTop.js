c6a0ecce19690a98b5812a21df4c29f4a6dacf71
angalda <angalda@mail.ru>
2022-08-21 11:57:08 +0300

    landing-top

--- src/components/LandinngTop/LandingTop.js
+++ src/components/LandinngTop/LandingTop.js
@@ -1,9 +1,12 @@
-// import logo from '../../images/logo.svg';
+import landingLogo from '../../images/landing-logo.svg';
 
 export default function LandingTop() {
-    return (
-        <div className="landing-top__container">
-          <h1>Учебный проект студента факультета Веб-разработки.<h1>
+  return (
+    <div className="landing-top__container">
+      <h2 className="landing-top__description">Учебный проект студента факультета Веб-разработки.</h2>
+      <div className="landing-top__img-container">
+        <img src={landingLogo} className="landing-top__img" />
       </div>
-    )
+    </div>
+  )
 }
