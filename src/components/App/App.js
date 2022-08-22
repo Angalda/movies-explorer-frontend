3b19cede011c8f8b233ef52c8a4aa02043d9e5a8
angalda <angalda@mail.ru>
2022-08-22 16:45:03 +0300

    feat: project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,15 @@
 
 import Header from '../Header/Header';
 import LandingTop from '../LandinngTop/LandingTop';
+import Project from '../Project/Project';
+
 
 function App() {
   return (
     <div className="page">
       <Header />
       <LandingTop />
+      <Project />
     </div>
   );
 }
