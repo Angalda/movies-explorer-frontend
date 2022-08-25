64270c4ad7c1a5af6bccd8d55d10e675e80607be
angalda <angalda@mail.ru>
2022-08-25 22:35:43 +0300

    feat: footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,8 @@ import Header from '../Header/Header';
 import LandingTop from '../LandinngTop/LandingTop';
 import Project from '../Project/Project';
 import Technology from '../Technology/Technology';
-import Student from '../Student/Student'
+import Student from '../Student/Student';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Project />
       <Technology />
       <Student />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer(){
+    return (
+        <footer className="footer__container">
+            <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className="footer__bottom">
+                <p className="footer__copyright">© 2020</p>
+                <ul className="footer__links-list">
+                    <li className="footer__link-item"><a href="#" className="footer__link">Яндекс.Практикум</a></li>
+                    <li className="footer__link-item"><a href="#" className="footer__link">Github</a></li>
+                    <li className="footer__link-item"><a href="#" className="footer__link">Facebook</a></li>
+
+                </ul>
+            </div>
+            
+        </footer>
+    )
+}
