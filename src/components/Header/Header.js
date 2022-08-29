88169421a0aba102bec4834b12674309056f493d
angalda <angalda@mail.ru>
2022-08-29 22:44:27 +0300

    fix: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import logo from '../../images/logo.svg';
 
 export default function Header(){
-    return (
-        <header className="header">
+  return (
+    <header className="header">
       <a href="/">
-        <img className="header__logo" src={logo} alt="logo"/>
+        <img className="header__logo" src={logo} alt="logo" />
       </a>
       <nav className='header__menu'>
         <ul className='header__menu-list'>
@@ -15,9 +15,9 @@ export default function Header(){
       <div className='header__aaccount'></div>
       <button className='header__burger-menu'></button>
       <div className='header__login'>
-      <button className='header__signup-btn'>Регистация</button>
-      <button className='header__signin-btn'>Войти</button>
-      </div>    
-      </header>
-    )
+        <button className='header__signup-btn'>Регистация</button>
+        <button className='header__signin-btn'>Войти</button>
+      </div>
+    </header>
+  )
 }
