3719e2f5a9b16f6b5a09f3105c801845a892eac8
Angalda <angalda@mail.ru>
2022-10-27 22:49:29 +0300

    Fix: getUser

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -35,6 +35,142 @@ function App() {
   const [listLength, setListLength] = useState(12);
   const [cardsNumber, setCardsNumber] = useState(4);
 
+
+/*Авторизация*/
+useEffect(() => {
+  if (authorized) {
+    mainApi.getProfile()
+      .then((res) => {
+        console.log(res.data);
+        setCurrentUser(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}, [authorized])
+
+useEffect(() => {
+  const jwt = localStorage.getItem("jwt")
+  if (jwt) {
+    
+    mainApi
+      .getProfile()
+      .then((res) => {
+        if (res) {
+          setAuthorized(true)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}, [history])
+
+function checkToken() {
+  if (localStorage.getItem("jwt")) {
+    const jwt = localStorage.getItem("jwt")
+    if (jwt) {
+      mainApi
+        .checkToken(jwt)
+        .then((data) => {
+          if (data) {
+            setAuthorized(true);
+            setCurrentUser(data);
+          }
+        })
+        .catch((err) => {
+
+          if (err === 401) {
+            setAuthorized(false)
+            console.log("401 - Токен не передан или передан не в том формате")
+          }
+          console.log("401 - Переданный токен не корректен")
+
+        })
+    }
+    if (!jwt) {
+      setAuthorized(false)
+    }
+  }
+}
+
+//Регистрация
+function handleRegister(name, email, password) {
+  mainApi
+    .register(name, email, password)
+    .then((res) => {
+      if (res) {
+        handleLogin(email, password)
+      }
+    })
+    .catch((err) => {
+      // setInfoTooltipOpen(true)
+      // setIsSymbol(false)
+      setMessage("Что-то пошло не так! Попробуйте ещё раз.");
+      if (err === 400) {
+        console.log("400 - некорректно заполнено одно из полей");
+      }
+
+    })
+}
+
+//Вход
+function handleLogin(password, email) {
+  mainApi
+    .login(email, password)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token)
+        handlePageLogin();
+        history.push("/movies");
+      }
+
+    })
+    .catch((err) => {
+      // setInfoTooltipOpen(true)
+      //  setIsSymbol(false)
+      setMessage("Что-то пошло не так! Попробуйте ещё раз.");
+      if (err === 400) {
+        console.log("400 - не передано одно из полей")
+      } else if (err === 401) {
+        console.log("401 - пользователь с email не найден")
+      }
+
+    })
+}
+
+function handlePageLogin() {
+  setAuthorized(true);
+  checkToken();
+}
+
+//Выход
+function handleLogout() {
+  setCurrentUser({})
+  setAuthorized(false)
+  localStorage.removeItem("jwt")
+  localStorage.clear()
+  history.push('/')
+}
+
+//Редактирование
+function handleEditProfile({ name, email }) {
+  mainApi
+    .editProfile(name, email)
+    .then((res) => {
+      setCurrentUser(res);
+      //setInfoTooltipOpen(true)
+      //setIsSymbol(true)
+      setMessage("Данные профиля успешно изменены.");
+    })
+    .catch((err) => {
+      console.log(err);
+      setMessage("Что-то пошло не так! Попробуйте ещё раз.");
+    });
+
+}
+
   function newWindowWidth() {
     setWidth(window.innerWidth);
 }
@@ -67,7 +203,7 @@ useEffect(() => {
    
    mainApi.getSaveMovies()
       .then((saveMovies) => {
-        console.log(currentUser._id)
+        console.log(currentUser)
         if(saveMovies) {
           //setSavedMovies(saveMovies.data);
          const moviesOfCurrentUser = saveMovies.data.filter(
@@ -114,140 +250,6 @@ function handleDeleteMovie (movie) {
   .catch((err) => console.log(err));
 }
 
-/*Авторизация*/
-  useEffect(() => {
-    if (authorized) {
-      mainApi.getProfile()
-        .then((res) => {
-          setCurrentUser(res.data)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  }, [authorized])
-
-  useEffect(() => {
-    const jwt = localStorage.getItem("jwt")
-    if (jwt) {
-      checkToken()
-    } else {
-      mainApi
-        .getProfile()
-        .then((res) => {
-          if (res) {
-            setAuthorized(true)
-          }
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  }, [history])
-
-  function checkToken() {
-    if (localStorage.getItem("jwt")) {
-      const jwt = localStorage.getItem("jwt")
-      if (jwt) {
-        mainApi
-          .checkToken(jwt)
-          .then((data) => {
-            if (data) {
-              setAuthorized(true);
-              setCurrentUser(data);
-            }
-          })
-          .catch((err) => {
-
-            if (err === 401) {
-              setAuthorized(false)
-              console.log("401 - Токен не передан или передан не в том формате")
-            }
-            console.log("401 - Переданный токен не корректен")
-
-          })
-      }
-      if (!jwt) {
-        setAuthorized(false)
-      }
-    }
-  }
-
-  //Регистрация
-  function handleRegister(name, email, password) {
-    mainApi
-      .register(name, email, password)
-      .then((res) => {
-        if (res) {
-          handleLogin(email, password)
-        }
-      })
-      .catch((err) => {
-        // setInfoTooltipOpen(true)
-        // setIsSymbol(false)
-        setMessage("Что-то пошло не так! Попробуйте ещё раз.");
-        if (err === 400) {
-          console.log("400 - некорректно заполнено одно из полей");
-        }
-
-      })
-  }
-
-  //Вход
-  function handleLogin(password, email) {
-    mainApi
-      .login(email, password)
-      .then((data) => {
-        if (data.token) {
-          localStorage.setItem("jwt", data.token)
-          handlePageLogin();
-          history.push("/movies");
-        }
-
-      })
-      .catch((err) => {
-        // setInfoTooltipOpen(true)
-        //  setIsSymbol(false)
-        setMessage("Что-то пошло не так! Попробуйте ещё раз.");
-        if (err === 400) {
-          console.log("400 - не передано одно из полей")
-        } else if (err === 401) {
-          console.log("401 - пользователь с email не найден")
-        }
-
-      })
-  }
-
-  function handlePageLogin() {
-    setAuthorized(true);
-    checkToken();
-  }
-
-//Выход
-  function handleLogout() {
-    setCurrentUser({})
-    setAuthorized(false)
-    localStorage.removeItem("jwt")
-    localStorage.clear()
-    history.push('/')
-  }
-
-  //Редактирование
-  function handleEditProfile({ name, email }) {
-    mainApi
-      .editProfile(name, email)
-      .then((res) => {
-        setCurrentUser(res);
-        //setInfoTooltipOpen(true)
-        //setIsSymbol(true)
-        setMessage("Данные профиля успешно изменены.");
-      })
-      .catch((err) => {
-        console.log(err);
-        setMessage("Что-то пошло не так! Попробуйте ещё раз.");
-      });
-
-  }
 
   return (
     <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
